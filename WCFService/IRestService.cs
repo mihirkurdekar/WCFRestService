b7aaf70c0c7af734ebff371bff6509d61a633d16
Mihir <mihir.kurdekar@gmail.com>
2017-05-13 18:16:57 +0530

    Solution for the project.

---
+++ WCFService/IRestService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRestService" in both code and config file together.
+    [ServiceContract]
+    public interface IRestService
+    {
+        [OperationContract]
+        [WebInvoke(Method="GET",UriTemplate="/add/{a}/{b}",RequestFormat=WebMessageFormat.Json,ResponseFormat=WebMessageFormat.Json,BodyStyle=WebMessageBodyStyle.Wrapped)]
+        string getAddition(string a,string b);
+    }
+}
---
+++ WCFService/UnitTest.Service.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NUnit.Framework;
+using WCFService;
+
+namespace UnitTestWCFService
+{
+	[TestFixture]
+	public class UnitTest
+	{
+        private static RestService _service;
+
+        public UnitTest()
+        {
+            _service = new RestService();
+        }
+		
+		[Test(Description="For additing 2 valid numbers")]
+        public void getAddition()
+        {
+			
+            string result=_service.getAddition("10", "12");
+            Assert.AreEqual("22", result);
+
+        }
+
+        [Test(Description="Addition Of 2 empty Strings")]
+        public void getAdditionEmptyInput()
+        {
+
+            string result = _service.getAddition("", "");
+            Assert.AreEqual("Some error occured", result);
+
+        }
+
+	}
+}
